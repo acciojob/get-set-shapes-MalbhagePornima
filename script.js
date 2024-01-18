839e61efc7c0856cabcf6315326b2bd771c4cdf4
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2024-01-18 17:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width2=width;
+		this.height2=height;
+	}
+	get width(){
+		return this.width2;
+	}
+	get height(){
+		return this.height2;
+	}
+	getArea(){
+		return this.height2*this.width2;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		super(side,side);
+	}
+	getPerimeter(){
+		return 4*this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
